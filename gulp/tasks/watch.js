662ec20a4d75809d6b5a2b88e71ed6e2a622274f
Lee Rogers <edwinleerogers@gmail.com>
2017-12-14 11:30:45 -0600

    complete header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,17 +3,24 @@ var gulp  = require('gulp'),
     browserSync = require('browser-sync').create();
 
 gulp.task('watch', function(){
+
   browserSync.init({
     notify: false,
     server: {
       baseDir: "app"
     }
   });
+
   watch('./app/index.html', function(){
     browserSync.reload();
   });
+
   watch('./app/assets/styles/**/*.css', function(){
     gulp.start('cssInject');
+  });
+
+  watch('./app/assets/scripts/**/*.js', function(){
+    gulp.start('scriptsRefresh');
   });
 });
 
@@ -21,3 +28,7 @@ gulp.task('cssInject', ['styles'], function(){
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+  browserSync.reload();
+})
